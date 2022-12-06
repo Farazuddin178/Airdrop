344cd3c81b0202e8e76f24ac207d204e10ff0784
Mohammed faraz uddin <mohammedfarazuddin01@gmail.com>
2022-12-06 10:07:20 +0000

    Create 4 files

---
+++ mahesh.js
@@ -0,0 +1,10 @@
+const Quote = require('inspirational-quotes');
+
+console.log(Quote.getQuote()); // returns quote (text and author)
+console.log(Quote.getQuote({ author: false }); // return quote without author
+console.log(Quote.getRandomQuote()); // return any random quote
+console.log(cowsay.say({
+    text :Quote. getQuote().text,
+    e : " OO",
+    T : "VV "
+}));
